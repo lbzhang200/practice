353d311b8e633599d5fbea92a2a17a8cc2d3bfbb
lbzhang200 <lbzhang20@gmail.com>
2026-06-09 10:11:27 +0800

    Rename folder to leet and move 176 to top-level

---
+++ 176/hw8/hw8.py
@@ -0,0 +1,31 @@
+def calculatetotalbeats(df):
+    beats = df["Tempo"] * df["Duration_ms"] / 60000
+    result_df = df["Artist", "Track"].copy()
+
+    result_df["Beats"] = beats 
+    return result_df 
+
+def checkSingleTrackConsistency(df):
+    mask = (df['Album_type'] == "single") & (df['Track'] != df['Album'])
+
+    result = df[mask]["Artist", "Track", "Album,", "Album_type"]
+    return result
+
+def isPrime(num):
+    if num < 2: 
+        return False
+    else:
+        for i in range(2, int(n**0.5)+1):
+            if (num % i == 0):
+                return False 
+            
+        return True 
+    
+def extractPrimeOccurence(df):
+    df = df.copy()
+    df['Occurence'] = df.groupby('Artist').cumcount() + 1 #total occruence of each track per artist
+    
+    mask = df['Occurence'].apply(is_Prime)
+    result = df[mask]["Artist", "Track", "Occurence"]
+
+    return result 
